69ce87fbdb7c66aa5c40caf6207cd1c1a14818fc
leslie-alldridge <leslie.alldridge@gmail.com>
2019-01-14 09:18:34 +1300

    reducer tests

---
+++ tests/client/reducers/goals.test.js
@@ -0,0 +1,50 @@
+import goals from "../../../src/reducers/goals";
+
+test("goals reducer Initial State", () => {
+  const expected = { isFetching: true };
+
+  const actual = goals(undefined, {});
+
+  expect(actual).toEqual(expected);
+});
+
+test("loading getting users goals", () => {
+  const expected = {"isFetching": true};
+
+  const action = {
+    type: "GOALS_LOADING",
+    isFetching: true
+  };
+
+  const actual = goals([], action);
+
+  expect(actual).toEqual(expected);
+});
+
+test('getting users goals', () => {
+    const goalsData = [{id: '1', goal: 'name'}];
+    const expected = [...goalsData];
+  
+    const action = {
+      type: 'GOALS_FETCH_DATA_SUCCESS',
+      isFetching: false,
+      goals: goalsData
+    };
+  
+    const actual = goals([], action).goals;
+  
+    expect(actual).toEqual(expected);
+  });
+
+  test("users goals errored", () => {
+    const expected = {"error": true};
+  
+    const action = {
+      type: "GOALS_HAS_ERRORED",
+      hasErrored: true
+    };
+  
+    const actual = goals([], action);
+  
+    expect(actual).toEqual(expected);
+  });
