182c0d33d835cc5dccbb0fbb0c74b10e1aead1a5
leslie-alldridge <leslie.alldridge@gmail.com>
2019-01-13 17:30:22 +1300

    updated db for postgres

--- migrations/20190112101746_goals.js
+++ migrations/20190112101746_goals.js
@@ -2,9 +2,9 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("goals", table => {
     table.increments("id");
     table.string("area");
-    table.integer("healthy");
-    table.integer("low");
-    table.integer("high");
+    table.float("healthy");
+    table.float("low");
+    table.float("high");
   });
 };
 
